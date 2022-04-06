5e02108d0ad442dd5b757b46c6d68f667a7b3ed3
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-04-06 17:43:30 +0000

    added about us

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,7 @@ import inputiconb from '../images/request tex-z icon.png'
 import inputiconc from '../images/Get assigned to a TM icon.png'
 import inputicond from '../images/Monitor TM icon.png'
 import inputicone from '../images/Delivered icon.png'
+import girl from '../images/3d About us lady.png'
 
 
 function Homepage(props) {
@@ -27,7 +28,7 @@ function Homepage(props) {
 
             {/* How we operate */}
 <div>
-    <div>
+    <div className={classes.howweoperatetitlecontainer}> 
          <div className={classes.howweoperatecontainer}> 
     <Dash title={'How We Operate'}  />
     <p>No <span className={classes.smallnowahala}>Wahala</span></p>
@@ -42,6 +43,21 @@ function Homepage(props) {
 <Stepsbox title={'Monitor TM '} inputicon={inputicond} stext={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros tellus, semper a efficitur sed, efficitur id diam.'}/>
 <Stepsbox title={'Delivered '} inputicon={inputicone} stext={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros tellus, semper a efficitur sed, efficitur id diam.'}/>
 </div>
+
+<div className={classes.aboutuscontainer}>
+    <Dash title={'About Tex-Z'}/>
+</div>
+<div className={classes.ladyandtextcon}>
+    <img src={girl} className={classes.lady}/>
+    <p className={classes.abouttext}>Tex-Z delivery inc. is a business that has been established in Lagos, Nigeria in the year 2020 and is reputed to be amongst the top best delivery brands in Nigeria by the year 2040. <br/> <br/>
+
+We intend to place much interest on delivering large sized goods with a considerable weight for both corporate and domestic clients, and we will strive to meet all the demands of our customers. <br/> <br/>
+
+Our delivery business has been established not only to generate revenue and make profit but to solve problems facing retailers and customers in movement of their goods and also to compete favorably with other goods delivery business in Lagos and in Nigeria. Tex-Z delivery inc. is aimed at striking a balance between retailers of large sized goods and its respective customers in terms of the supposed goods movements. By so doing, saves the retailer and customer time, cost and energy.
+</p>
+</div>
+
+
 
 
 
