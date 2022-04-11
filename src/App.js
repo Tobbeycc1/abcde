b5e13c55668fdaeae8c0a0cfa6e6c0cdb95a9a43
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-04-11 03:08:58 +0000

    Modal added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import { Routes, Route } from "react-router-dom";
+import BecomeAPartner from "./componet/BecomeAPartner";
 import Header from "./componet/header";
 import Homepage from "./pages/HomePage";
 import Livetrack from "./pages/livetrack";
+import { useState } from "react";
 
 function App() {
+const [modalIsOpen, setModalisOpen]= useState(false)
+
+  function Show() {
+    console.log('jfgfhfghfg');
+    setModalisOpen(true)
+}
  return(
  <div>
-  <Header/>
+  {modalIsOpen &&  <BecomeAPartner/>}
+  <Header onClick={Show}/>
   <Routes>
     <Route path="/" element={<Homepage/>}/>
     <Route path="/live-track" element={<Livetrack/>}/>
